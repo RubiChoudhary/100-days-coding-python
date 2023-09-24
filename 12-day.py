7c6852496f6fa2eeca12972f26fe945e1b76aba7
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-09-25 01:11:23 +0530

    Create 12-day.py

---
+++ 12-day.py
@@ -0,0 +1,14 @@
+n=int(input())
+for i in range(0, n):
+    for j in range(0, i+1):
+        print("*", end=' ')
+    print()
+for i in range( 0,n,1):
+    for j in range(0, i+1):
+        print("*", end=' ')
+    print()
+for i in range(0, n):
+    for j in range(1):
+        print("*", end=' ')
+    print()
+        
