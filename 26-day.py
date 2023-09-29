8fba3d8797c2d5d4853179561f2c30c2a586f6ee
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-09-29 21:48:23 +0530

    Create 26-day.py
    Christmas Tree Star pattern

---
+++ 26-day.py
@@ -0,0 +1,23 @@
+n = int(input())
+for i in range(0,n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for j in range(0,i+1):
+        print("*",end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+for i in range(0,n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for j in range(0,i+1):
+        print("*",end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+for i in range(0, n):
+    for j in range(0,n-2,1):
+        print(" ", end=' ')
+    for j in range(0,3,1):
+        print("*", end=' ')
+    print()
