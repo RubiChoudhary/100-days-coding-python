91aef1aab415527ef770c7898b77d671c5001c1d
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-11-15 01:18:30 +0530

    Create 71-day.py
    Write Program to find sum of elements in an array
    Description
    Get an array as the input from the user and find the sum of the elements.
    Strictly follow the input and output format. 
    
    Input
    Enter the size of array
    3
    Enter the elements of array
    5 10 15
    Output
    30

---
+++ 71-day.py
@@ -0,0 +1,24 @@
+def solution():
+  size = int(input())
+  elements = input().split()
+  elements = [int(x) for x in elements]
+  sum_of_elements = sum(elements)
+  print(sum_of_elements)
+  pass
+
+solution()
+
+
+
+# Write Program to find sum of elements in an array
+# Description
+# Get an array as the input from the user and find the sum of the elements.
+# Strictly follow the input and output format. 
+
+# Input
+# Enter the size of array
+# 3
+# Enter the elements of array
+# 5 10 15
+# Output
+# 30
