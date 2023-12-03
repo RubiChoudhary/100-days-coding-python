3518e49bb1f48c17476722dc23e81d41984e7db1
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-12-03 19:52:47 +0530

    Create 89-day.py
    Panjak Bhaiya's Pointer Question

---
+++ 89-day.py
@@ -0,0 +1,12 @@
+num = [1, 2, 4, 5]
+k = 5
+sum1 = 0
+summing =0
+for i in range(1,k+1):
+    summing   = summing + i 
+print(summing)
+for j in num:
+    sum1 = sum1+j
+    if summing != sum1:
+        missing_number =summing - sum1
+print("missing number"missing_number)
